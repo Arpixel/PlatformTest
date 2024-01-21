29fde70adecf1ea795735f84587439627d898cfb
Slowerka <slowerkaoriginal@outlook.com>
2024-01-21 18:19:52 +0300

    Düşam kodu

--- Script/Karakter.gd
+++ Script/Karakter.gd
@@ -12,10 +12,12 @@ func _process(_delta):
 		dir = 1
 	else:
 		dir = 0
+
 	if Input.is_action_pressed("A"):
 		dir = -1
 	if Input.is_action_pressed("ui_select") and is_on_floor():
 		Velocity.y = JumpPower
+
 	Velocity.x = speed * dir
 	Velocity.y += Gravity
 	Velocity = move_and_slide(Velocity, Vector2.UP)
