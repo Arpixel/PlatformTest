f290939d70257722f608eeec91841fdf7a181439
Eka <mycraft323@gmail.com>
2024-01-21 17:30:28 +0300

    karakter eklendi

---
+++ Script/Karakter.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var Velocity = Vector2()
+var dir = 0
+var Gravity = 15
+var speed = 200
+var JumpPower = -600
+
+
+func _process(delta):
+	if Input.is_action_pressed("D"):
+		dir = 1
+	else:
+		dir = 0
+	if Input.is_action_pressed("A"):
+		dir = -1
+	if Input.is_action_pressed("ui_select") and is_on_floor():
+		Velocity.y = JumpPower
+	Velocity.x = speed * dir
+	Velocity.y += Gravity
+	move_and_slide(Velocity, Vector2.UP)
